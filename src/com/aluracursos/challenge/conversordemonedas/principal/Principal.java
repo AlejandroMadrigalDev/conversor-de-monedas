8230a3e90dd19bb143a3aeed1e12630c86606467
AlejandroMadrigalDev <alejandromadrigal_1999@outlook.com>
2026-01-29 19:34:55 -0500

    Archivos Iniciales

---
+++ src/com/aluracursos/challenge/conversordemonedas/principal/Principal.java
@@ -0,0 +1,7 @@
+package com.aluracursos.challenge.conversordemonedas.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hi, new project! <3");
+    }
+}
